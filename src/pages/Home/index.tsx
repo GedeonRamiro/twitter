ce280363275f08b8dd8719724a0146bda47ce7b7
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2021-10-02 08:27:48 -0300

    feat(Home): add main components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,14 +4,64 @@ import { useGlobalState } from "../../context/GlobalContext";
 
 import Sidebar from "../SideBar";
 import SeachInput from "../../components/SearchInput";
+import Main from "../../components/Main";
+import { Container } from './styled'
 
 const Home: React.FC = () => {
   
   return (
-    <>
-      {/* <Sidebar /> */}
+    <Container>
+      <Sidebar />
+          <Main fixedContent={
+            <h1 style={{paddingTop: "15px", paddingBottom: "15px"}}>Gedeon Ramiro</h1>
+          }>
+             <h1>Meu primeiro Tweet!</h1> 
+             <h1>Meu segundo Tweet!</h1> 
+             <h1>Meu terceiro Tweet!</h1>
+             <div style={{paddingTop: "50px", paddingBottom: "50px"}}></div> 
+             <h1>Meu primeiro Tweet!</h1> 
+             <h1>Meu segundo Tweet!</h1> 
+             <h1>Meu terceiro Tweet!</h1>
+             <div style={{paddingTop: "50px", paddingBottom: "50px"}}></div> 
+             <h1>Meu primeiro Tweet!</h1> 
+             <h1>Meu segundo Tweet!</h1> 
+             <h1>Meu terceiro Tweet!</h1>
+             <div style={{paddingTop: "50px", paddingBottom: "50px"}}></div> 
+             <h1>Meu primeiro Tweet!</h1> 
+             <h1>Meu segundo Tweet!</h1> 
+             <h1>Meu terceiro Tweet!</h1>
+             <div style={{paddingTop: "50px", paddingBottom: "50px"}}></div> 
+             <h1>Meu primeiro Tweet!</h1> 
+             <h1>Meu segundo Tweet!</h1> 
+             <h1>Meu terceiro Tweet!</h1>
+             <div style={{paddingTop: "50px", paddingBottom: "50px"}}></div> 
+             <h1>Meu primeiro Tweet!</h1> 
+             <h1>Meu segundo Tweet!</h1> 
+             <h1>Meu terceiro Tweet!</h1>
+             <div style={{paddingTop: "50px", paddingBottom: "50px"}}></div> 
+             <h1>Meu primeiro Tweet!</h1> 
+             <h1>Meu segundo Tweet!</h1> 
+             <h1>Meu terceiro Tweet!</h1>
+             <div style={{paddingTop: "50px", paddingBottom: "50px"}}></div> 
+             <h1>Meu primeiro Tweet!</h1> 
+             <h1>Meu segundo Tweet!</h1> 
+             <h1>Meu terceiro Tweet!</h1>
+             <div style={{paddingTop: "50px", paddingBottom: "50px"}}></div> 
+             <h1>Meu primeiro Tweet!</h1> 
+             <h1>Meu segundo Tweet!</h1> 
+             <h1>Meu terceiro Tweet!</h1>
+             <div style={{paddingTop: "50px", paddingBottom: "50px"}}></div> 
+             <h1>Meu primeiro Tweet!</h1> 
+             <h1>Meu segundo Tweet!</h1> 
+             <h1>Meu terceiro Tweet!</h1>
+             <div style={{paddingTop: "50px", paddingBottom: "50px"}}></div> 
+             <h1>Meu primeiro Tweet!</h1> 
+             <h1>Meu segundo Tweet!</h1> 
+             <h1>Meu terceiro Tweet!</h1>
+             <div style={{paddingTop: "50px", paddingBottom: "50px"}}></div> 
+          </Main>
       <SeachInput />
-    </>
+    </Container>
   )
 }
 
