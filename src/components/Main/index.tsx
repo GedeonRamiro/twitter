cef20376d9f565a3125fe5dc98c69c9177f4bede
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2021-10-02 08:27:14 -0300

    feat Main Comnponents

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,17 @@
+import { Children } from 'react'
+import { Container, FixedContent, ScrollableContainer } from './styles'
+
+interface IProps {
+    fixedContent?: JSX.Element
+}
+
+const Main: React.FC<IProps> = ( { children, fixedContent } ) => {
+    return (
+        <Container>
+           <FixedContent>{fixedContent}</FixedContent> 
+            <ScrollableContainer>{children}</ScrollableContainer>
+        </Container>
+    )
+}
+
+export default Main
---
+++ src/components/Main/styles.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    width: 600px;
+    height: 100vh;
+    border-left: 1px solid ${props => props.theme.color.borderColor};
+    border-right: 1px solid ${props => props.theme.color.borderColor};
+    overflow: auto;
+
+    &::-webkit-scrollbar {
+        width: 0px;
+        background: transparent;
+    }
+`
+
+
+export const FixedContent = styled.div`
+    position: sticky;
+    top: 0;
+    background-color: #000;
+    border-bottom: 1px solid ${props => props.theme.color.borderColor};
+    padding-left: 20px;
+    padding-right: 20px;
+`
+
+export const ScrollableContainer = styled.div`
+    padding-left: 20px;
+    padding-right: 20px;
+
+`
