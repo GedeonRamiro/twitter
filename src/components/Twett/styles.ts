f227d8974a42f380979d766919c191ff8bb35cad
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2021-10-03 12:00:34 -0300

    feat(Home): create Tweet

--- src/components/Twett/styles.ts
+++ src/components/Twett/styles.ts
@@ -18,15 +18,17 @@ export const Container = styled.div`
 export const TweetText = styled.div`
     font-size: 12px;
     line-height: 14px;
+    margin-left: 50px;
 `
 export const TweetNames = styled.div`
     display: flex;
+    padding-bottom: 5px;
 
     h1{
         font-size: 15px;
         line-height: 18px;
         font-weight: 700;
-         margin-left: 10px;
+        margin-left: 10px;
 
     }
 
