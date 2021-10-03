2f88bf33bc78a93f8ac49c5d15546e4f45fcfb4d
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2021-10-03 11:00:48 -0300

    feat(Home):  integrate feed with backend

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,41 @@
 
-import { apiWithAuth } from "../../services/api";
+import api, { apiWithAuth } from "../../services/api";
 import { IAuth, useGlobalState } from "../../context/GlobalContext";
 
 import PageWrapper from "../../components/PageWrapper";
 import { TweetButton, TweetContainer, TweetInput, UserName } from "./styles";
 import Button from "../../components/Button";
 import Tweet from "../../components/Twett";
+import { useEffect, useState } from "react";
 
+interface ITweet {
+      id: string,
+      content: string,
+      user: {
+        id: string,
+        name: string,
+        username: string,
+        email: string,
+      },
+      created_at: string,
+}
 
 const Home = () => {
 
+      const [tweets, setTweets] = useState<ITweet[]>([])
+
   const { auth: {user} } = useGlobalState() as {auth: IAuth}
+
+  const getTweet = async () => {
+
+      const { data } = await apiWithAuth.get<ITweet[]>('/feed')
+      setTweets(data)
+  }
+
+  useEffect(() => {
+        getTweet()
+  }, [])
+
 
   return (
 
@@ -28,73 +53,12 @@ const Home = () => {
       </TweetButton>
       </>
     }>
-      <Tweet name={user.name} username={user.username}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Rerum suscipit hic officiis debitis, harum quas commodi 
-            voluptas aliquid numquam nostrum. Corporis deserunt officia 
-            voluptates tempore distinctio aliquam quos incidunt commodi?
+      {tweets.map(tweet =>(
+      <Tweet name={tweet.user.name} username={tweet.user.username} key={tweet.id}>
+           {tweet.content}
       </Tweet>
-      <Tweet name={user.name} username={user.username}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Rerum suscipit hic officiis debitis, harum quas commodi 
-            voluptas aliquid numquam nostrum. Corporis deserunt officia 
-            voluptates tempore distinctio aliquam quos incidunt commodi?
-      </Tweet>
-      <Tweet name={user.name} username={user.username}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Rerum suscipit hic officiis debitis, harum quas commodi 
-            voluptas aliquid numquam nostrum. Corporis deserunt officia 
-            voluptates tempore distinctio aliquam quos incidunt commodi?
-      </Tweet>
-      <Tweet name={user.name} username={user.username}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Rerum suscipit hic officiis debitis, harum quas commodi 
-            voluptas aliquid numquam nostrum. Corporis deserunt officia 
-            voluptates tempore distinctio aliquam quos incidunt commodi?
-      </Tweet>
-      <Tweet name={user.name} username={user.username}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Rerum suscipit hic officiis debitis, harum quas commodi 
-            voluptas aliquid numquam nostrum. Corporis deserunt officia 
-            voluptates tempore distinctio aliquam quos incidunt commodi?
-      </Tweet>
-      <Tweet name={user.name} username={user.username}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Rerum suscipit hic officiis debitis, harum quas commodi 
-            voluptas aliquid numquam nostrum. Corporis deserunt officia 
-            voluptates tempore distinctio aliquam quos incidunt commodi?
-      </Tweet>
-      <Tweet name={user.name} username={user.username}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Rerum suscipit hic officiis debitis, harum quas commodi 
-            voluptas aliquid numquam nostrum. Corporis deserunt officia 
-            voluptates tempore distinctio aliquam quos incidunt commodi?
-      </Tweet>
-      <Tweet name={user.name} username={user.username}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Rerum suscipit hic officiis debitis, harum quas commodi 
-            voluptas aliquid numquam nostrum. Corporis deserunt officia 
-            voluptates tempore distinctio aliquam quos incidunt commodi?
-      </Tweet>
-      <Tweet name={user.name} username={user.username}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Rerum suscipit hic officiis debitis, harum quas commodi 
-            voluptas aliquid numquam nostrum. Corporis deserunt officia 
-            voluptates tempore distinctio aliquam quos incidunt commodi?
-      </Tweet>
-      <Tweet name={user.name} username={user.username}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Rerum suscipit hic officiis debitis, harum quas commodi 
-            voluptas aliquid numquam nostrum. Corporis deserunt officia 
-            voluptates tempore distinctio aliquam quos incidunt commodi?
-      </Tweet>
-      <Tweet name={user.name} username={user.username}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Rerum suscipit hic officiis debitis, harum quas commodi 
-            voluptas aliquid numquam nostrum. Corporis deserunt officia 
-            voluptates tempore distinctio aliquam quos incidunt commodi?
-      </Tweet>
-             
+      ))}
+     
       </ PageWrapper>
   )
 }
