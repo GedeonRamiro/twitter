02bb9664eca7d4a330a102bb0a7eb5c7a413e104
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2021-10-07 10:12:15 -0300

    feat(Perfil): profile section perfil page

--- src/pages/Perfil/index.tsx
+++ src/pages/Perfil/index.tsx
@@ -1,8 +1,9 @@
 import PageWrapper from "../../components/PageWrapper"
 import {BsArrowLeft} from 'react-icons/bs'
+import {FaRegCalendarAlt} from 'react-icons/fa'
 import { Link } from "react-router-dom"
-import { FixedContentContainer, FixedContentTexts } from "./styles"
-
+import { Bio, CreateAt, FixedContentContainer, FixedContentTexts, Follower, ImageContainer, Name, TextContainer, UserName } from "./styles"
+import Button from "../../components/Button"
 
 
 const Perfil = () => {
@@ -22,7 +23,24 @@ const Perfil = () => {
         }
       >
 
-        Página de perfil
+        <ImageContainer>
+            <img src={`https://lorempixel.com/400/400/cats/1/`} alt="1" />
+            <Button>Editar perfil</Button>
+        </ImageContainer>
+        <TextContainer>
+          <Name>Gedeon Ramiro</Name>
+          <UserName>@gedeon_ramiro</UserName>
+          <Bio>CTO | Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus sed amet nihil saepe maiores? Maxime quasi nobis, fuga iure accusantium hic eum iste sunt ea tempora cumque ipsam eos ducimus.</Bio>
+          <CreateAt>
+            <FaRegCalendarAlt size={18}/>
+            <p>Ingressou em janeiro de 2021</p>
+          </CreateAt>  
+            <Follower>
+              <p>500 <span>seguindo</span></p>
+              <p>3 <span>seguidores</span></p>
+            </Follower>
+        </TextContainer>
+      
       </PageWrapper>
 
       )
