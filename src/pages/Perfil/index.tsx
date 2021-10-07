2f5e8cdce00ae4b70d4ad867d79527e45ac149d5
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2021-10-07 08:52:02 -0300

    feat(Perfil): create fixed content

--- src/pages/Perfil/index.tsx
+++ src/pages/Perfil/index.tsx
@@ -1,9 +1,30 @@
-import Sidebar from "../../components/SideBar"
+import PageWrapper from "../../components/PageWrapper"
+import {BsArrowLeft} from 'react-icons/bs'
+import { Link } from "react-router-dom"
+import { FixedContentContainer, FixedContentTexts } from "./styles"
+
 
 
 const Perfil = () => {
     return (
-        <Sidebar />
+      
+      <PageWrapper
+        fixedContent={
+          <FixedContentContainer>
+            <Link to='/'>
+                <BsArrowLeft size={25} />
+            </Link>
+            <FixedContentTexts>
+              <h1>Olá</h1>
+              <h2>11 Tweets</h2>
+            </FixedContentTexts>  
+          </FixedContentContainer>
+        }
+      >
+
+        Página de perfil
+      </PageWrapper>
+
       )
 }
 
