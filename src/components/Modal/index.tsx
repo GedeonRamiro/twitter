3e31fd32e293743fade1642464af386f079ed11f
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2021-10-09 15:35:52 -0300

    fix(Modal): tot and left distances in position absolute

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -11,7 +11,7 @@ const Modal:React.FC<IProps> = ({ children, isOpen, setIsOpen, onClose }) => {
     const handleOnClose = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
         if(event.target === event.currentTarget){
             setIsOpen(false)
-            onClose!()
+            onClose && onClose()
         }
     }
 
