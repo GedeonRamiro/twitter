65db0e11378234794104015b7f4a19e2731fbf1f
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2021-10-14 09:08:59 -0300

    feat(Seguidores): integrate with backend to get follows and followers

--- src/pages/Seguidores/index.tsx
+++ src/pages/Seguidores/index.tsx
@@ -10,10 +10,38 @@ import {
     UserText
 } from './style'
 import { IAuth, useGlobalState } from "../../context/GlobalContext"
+import { useEffect, useState } from "react"
+import { apiWithAuth } from "../../services/api"
+
+interface IUser {
+        id: string,
+        name: string,
+        bio: string | null,
+        username: string,
+        email: string,
+}
+
+interface IFollows{
+    follows: IUser[]
+    followers: IUser[]
+}
 
 const Seguidores = () => {
 
     const { auth: {user} } = useGlobalState() as {auth: IAuth}
+
+    const [data, setData] = useState<IFollows>()
+    const [showFollows, setShowFollows] = useState(true)
+    console.log(data?.followers)
+
+    const getFollows = async () => {
+        const { data } = await apiWithAuth.get<IFollows>('profile/follows')
+        setData(data)
+    }
+
+    useEffect(() => {
+        getFollows()
+    }, [])
 
     return (
         <PageWrapper
@@ -28,26 +56,47 @@ const Seguidores = () => {
                     </FixedContentTexts>  
                 </FixedContentContainer>
                 <FollowerContainer>
-                    <FollowerTitle isAction={true}>
+                    <FollowerTitle isAction={showFollows} onClick={() => setShowFollows(true)}>
                         Seguidores
                     </FollowerTitle>
-                    <FollowerTitle isAction={false}>
+                    <FollowerTitle isAction={!showFollows} onClick={() => setShowFollows(false)}>
                         Seguindo
                     </FollowerTitle>
                 </FollowerContainer>
             </>    
             }
         >
-          <Link to={`/perfil/${user.username}`}>
-            <UserContainer>
-                <img src={`https://robohash.org/${user.username}/`}  alt={user.name}/>
-                <UserText>
-                    <h1>{user.name}</h1>
-                    <h2>@{user.username}</h2>
-                    <p>CTO | Front-end</p>
-                </UserText>
-            </UserContainer>
-          </Link>  
+
+          {showFollows ? (
+            
+            data?.followers.map(followers => (
+                <Link to={`/perfil/${followers.username}`} key={followers.id}>
+                    <UserContainer>
+                        <img src={`https://robohash.org/${followers.username}/`}  alt={followers.name}/>
+                        <UserText>
+                            <h1>{followers.name}</h1>
+                            <h2>@{followers.username}</h2>
+                            <p>{followers.bio}</p>
+                        </UserText>
+                    </UserContainer>
+                </Link>  
+            ))
+
+            ) : (
+            data?.follows.map(follows => (
+                <Link to={`/perfil/${follows.username}`} key={follows.id}>
+                    <UserContainer>
+                        <img src={`https://robohash.org/${follows.username}/`}  alt={follows.name}/>
+                        <UserText>
+                            <h1>{follows.name}</h1>
+                            <h2>@{follows.username}</h2>
+                            <p>{follows.bio}</p>
+                        </UserText>
+                    </UserContainer>
+                </Link>   
+
+            ))
+        )} 
 
         </PageWrapper>
     )
